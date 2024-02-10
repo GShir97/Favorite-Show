c086e9b5b24291047044ce733caa7c14fa2556b5
GShir97 <goldshir97@gmail.com>
2024-02-10 15:23:14 +0200

    2

--- app/src/main/java/com/example/favshow/myData.java
+++ app/src/main/java/com/example/favshow/myData.java
@@ -4,7 +4,7 @@ public class myData {
     static String[] nameArray ={"SpongeBob", "PatrickStar", "Squidward", "MrKrabs",
             "Plankton", "Sandy", "MrsPuff", "Gary", "PearlKrabs", "Larry" };
     static String[] description = {"SpongeBob SquarePants is a yellow sea sponge. He lives in a pineapple house and is employed as a fry cook at a fast food restaurant called the Krusty Krab.",
-    "Patrick Star is a pink starfish who lives under a rock. He is best friends with SpongeBob and often unknowingly encourages activities that get the two into trouble.",
+    "Patrick Star is a pink starfish who lives under a rock. He is best friends with SpongeBob and often encourages activities that get the two into trouble.",
     "Squidward Tentacles is a turquoise Giant Pacific octopus who works as a cashier at the Krusty Krab. He is SpongeBob's next-door neighbor.",
     "Eugene Krabs is a red crab who owns and operates the Krusty Krab restaurant. He dislikes spending money.",
     "Plankton is a small planktonic copepod. His ultimate goal is to steal Krabs' secret formula for Krabby Patties.",
